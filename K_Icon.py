39904c25736aae8b8151f00eb4157bf0d96e1047
vishnu <vishnunvs0003@gmail.com>
2025-07-07 22:00:22 +0530

    update

--- K_Icon.py
+++ K_Icon.py
@@ -1,6 +1,6 @@
 print("\n")
 for row in range(7):
-    for col in range(7):
+    for col in range(5):
         if col==0 or row+col==4 or row - col == 2 :
             print("*",end=" ")
         else:
